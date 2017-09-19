40dd9625c119ec5fc0ad7d6660eb19a2169f26e6
Eduardo Moroni <eduardomoronI@gmail.com>
2017-09-19 17:22:04 -0300

    ci: configurar regras do danger (#6)
    * ci: comentar error de lint atraves do danger
    
    * ci: comentar tests que foram ignorados
    
    * ci: danger ira comentar caso o PR seja sucesso
    
    * revert: revertendo erros propositais de linter

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,56 @@
-import { message, danger } from 'danger'
+import { danger, markdown } from 'danger'
+import _ from 'lodash'
+import fs from 'fs'
 
-message(':tada:, this worked @' + danger.github.pr.user.login)
+let errorsAndWarnings = 0
+
+checkLinterErrors()
+checkSkippedTests()
+isABigPR()
+congrats()
+
+function checkSkippedTests() {
+  const modifiedSpecFiles =  danger.git.modified_files.filter(filePath => filePath.match(/.test.(js|jsx)$/gi))
+
+  const testFilesIncludeExclusion = modifiedSpecFiles.reduce((acc, value) => {
+    const content = fs.readFileSync(value).toString()
+    const invalid = _.includes(content, 'it.only') || _.includes(content, 'describe.only') || _.includes(content, 'fdescribe') || _.includes(content, 'fit(')
+    if (invalid) { acc.push(value) }
+    return acc
+  }, [])
+
+  if (testFilesIncludeExclusion.length > 0) {
+    errorsAndWarnings++
+    fail(`skipped test was left in tests (${testFilesIncludeExclusion})`)
+  }
+}
+
+function checkLinterErrors() {
+  const linterOutput = fs.readFileSync('prepush.log').toString()
+  const thereWarningOrErrors = _.includes(linterOutput, 'error') || _.includes(linterOutput, 'warning')
+
+  if (thereWarningOrErrors) {
+    errorsAndWarnings++
+    markdown('O linter está reclamando sobre: ')
+
+    _.each(linterOutput.split('\n'), (line) => {
+      if (!_.startsWith(line, '>')) {
+        markdown(line)
+      }
+    })
+  }
+}
+
+function isABigPR() {
+  const bigPRThreshold = 1000
+  if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+    errorsAndWarnings++
+    warn(':exclamation: Big PR')
+  }
+}
+
+function congrats() {
+  if (!errorsAndWarnings) {
+    message(`Nenhum error ou warning @${danger.github.pr.user.login} :tada:`)
+  }
+}
